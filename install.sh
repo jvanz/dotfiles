58378d7f95715d70d75d45f5b1d0b5777a0dae8a
José Guilherme Vanz <guilherme.sft@gmail.com>
2017-08-30 22:17:09 -0300

    tmux: remove fedora.conf
    Keep only one configuration file for tmux.
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- install.sh
+++ install.sh
@@ -3,13 +3,12 @@
 dirname=$(dirname $(readlink -f $0))
 
 sudo dnf update -y
-sudo dnf install -y vim tmux
+sudo dnf install -y vim tmux powerline tmux-powerline
 
 rm $HOME/{.vim,.gitconfig,.tmux.conf,.fedora.conf}
 
 ln -s $dirname/.vim $HOME/.vim
 ln -s $dirname/.gitconfig $HOME/.gitconfig
 ln -s $dirname/tmux/.tmux.conf $HOME/.tmux.conf
-ln -s $dirname/tmux/.fedora.conf $HOME/.fedora.conf
 
 echo "source $dirname/.bashrc" >> $HOME/.bashrc
